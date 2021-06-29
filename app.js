4de85c0158920c756991231a9f8ea25d3387c5e1
酷酷男孩 <2294211995@qq.com>
2021-06-29 10:42:03 +0800

    修改端口

--- app.js
+++ app.js
@@ -123,7 +123,7 @@ fs.readdirSync(path.join(__dirname, 'module'))
     })
   })
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8989
 const host = process.env.HOST || ''
 
 app.server = app.listen(port, host, () => {
